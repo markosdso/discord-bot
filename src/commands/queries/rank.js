4e07b9af73042d536a85a616588f14552c11ff4d
markydso <markosdeieso@gmail.com>
2025-01-07 18:42:08 -0500

    Added optional conditionals to rank command

--- src/commands/queries/rank.js
+++ src/commands/queries/rank.js
@@ -2,6 +2,8 @@ const { ApplicationCommandOptionType, ButtonStyle } = require('discord.js');
 const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
 const Ranking = require('../../models/ranking');
 const User = require('../../models/user');
+const Rollercoaster = require('../../models/rollercoaster');
+const { QueryTypess, Sequelize } = require('sequelize');
 
 module.exports = {
     name: 'rank',
@@ -12,6 +14,42 @@ module.exports = {
             description: 'user id',
             type: ApplicationCommandOptionType.User,
             required: 'true'
+        },
+        {
+            name: 'where',
+            description: 'variable in consideration',
+            type: ApplicationCommandOptionType.String,
+            choices: [
+                {
+                    name: 'park',
+                    value: 'park'
+                },
+                {
+                    name: 'manufacturer',
+                    value: 'manufacturer',
+                },
+                {
+                    name: 'model',
+                    value: 'model',
+                },
+                {
+                    name: 'country',
+                    value: 'country'
+                },
+                {
+                    name: 'seating',
+                    value: 'seating'
+                },
+                {
+                    name: 'type',
+                    value: 'type'
+                }
+            ]
+        },
+        {
+            name: 'is',
+            description: 'value',
+            type: ApplicationCommandOptionType.String,
         }
     ],
 
@@ -42,35 +80,94 @@ module.exports = {
                 return;
             }
 
-            const rankings = await Ranking.findAll({ 
-                where: {id: id},
-                order: [
-                    ['rank', 'ASC']
-                ]
-            });
+            let rankings;
+            let subset = false;
+
+            if (interaction.options.get('where') && interaction.options.get('is')) {
+                subset = true;
+
+                const attr = interaction.options.get('where').value;
+                const cond = interaction.options.get('is').value;
+
+                const filteredChoices = await Rollercoaster.findAll({
+                    where: {
+                        [attr]: cond,
+                    }
+                })
+
+                let names = [];
+                let parks = [];
+
+                if (filteredChoices.length > 0){
+                    names = filteredChoices.map((choice) => {
+                        return choice.name;
+                    });
+    
+                    parks = filteredChoices.map((choice) => {
+                        return choice.park;
+                    });
+                }
+
+                rankings = await Ranking.findAll({ 
+                    where: {
+                        id: id,
+                        name: names,
+                        park: parks,
+                    },
+                    order: [
+                        ['rank', 'ASC']
+                    ]
+                });
+                  
+
+            } else {
+                rankings = await Ranking.findAll({ 
+                    where: {id: id},
+                    order: [
+                        ['rank', 'ASC']
+                    ]
+                });
+            }
 
 
-            const generateEmbed = start => {
+            const generateEmbed = async start => {
                 const cur = rankings.slice(start, start + 10)
 
                 curPage = Math.floor((start / 10) + 1);
                 pageTotal = Math.ceil((rankings.length / 10))
 
                 let output = '';
-                for(ranking of cur){
-                    output += `${ranking.rank}. ${ranking.name}, ${ranking.park} \n`;
+                let i = 0;
+                for(let i = 0; i < cur.length; i++){
+                    output += `${i + start + 1}. ${cur[i].name}, ${cur[i].park} \n`;
                 }
+
+                if (subset) {
+                    output = "of " + interaction.options.get('is').value + "\n \n" + output;
+                }
+
+                const rollercoaster = await Rollercoaster.findOne({
+                    where: {
+                        name: cur[0].name,
+                        park: cur[0].park,
+                    },
+                })
 
                 return new EmbedBuilder()
                     .setTitle(`${user.name}'s (@${user.tag}) ranking\n 📄  (${curPage}/${pageTotal})`)
                     .setDescription(output)
                     .setColor('DarkOrange')
+                    .setThumbnail(rollercoaster.imgSrc)
+            }
 
+            if (rankings.length == 0) {
+                interaction.reply("No rollercoasters found with specified condition");
+                return;
             }
 
             const canFitOnOnePage = rankings.length <= 10
             const embedMessage = await interaction.reply({
-                embeds: [generateEmbed(0)],
+                embeds: [await generateEmbed(0)],
                 components: canFitOnOnePage
                     ? []
                     : [new ActionRowBuilder().setComponents([forwardButton])]
@@ -79,7 +176,6 @@ module.exports = {
             if (canFitOnOnePage) return;
 
             const collector = embedMessage.createMessageComponentCollector({
-                filter: ({member}) => member.id === user.id
             })
 
             let idx = 0;
@@ -95,11 +191,6 @@ module.exports = {
                     ]
                 })
             })
-
-
-
-
-
 
 
         } catch (error) {
